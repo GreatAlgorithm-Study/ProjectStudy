6337b52b12ec766b3aec02b69a11df94756839e5
KodaHye <kodhdh09@gmail.com>
2024-12-18 04:08:43 +0900

    feat: 로그인 화면 구현

--- talktalk_fe/src/index.js
+++ talktalk_fe/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // React Router 적용
+  // App을 BrowserRouter로 감싸면서 프로젝트 페이지들을 URL경로에 따라 렌더링할 수 있도록 함
+  // 컴포넌트에 Route로 라우팅시켜야됨
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
