d7edabd0401a04753fd114466a42758945f5fee7
KodaHye <kodhdh09@gmail.com>
2024-12-19 22:53:44 +0900

    chore: config 설정 추가

---
+++ 고다혜_TalkTalk/TalkTalk_BE/src/main/java/com/example/talktalk_be/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.talktalk_be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // Spring 서버 전역적으로 CORS 설정
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
