a6c5fafed875baf1e724d93e62825d05ce7b0fe9
KodaHye <72763127+KodaHye@users.noreply.github.com>
2025-01-08 00:56:39 +0900

    delete: 고다혜_TalkTalk/TalkTalk_BE/src directory

--- 고다혜_TalkTalk/TalkTalk_BE/src/test/java/com/example/talktalk_be/TalkTalkBeApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.example.talktalk_be;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TalkTalkBeApplicationTests {
-}
